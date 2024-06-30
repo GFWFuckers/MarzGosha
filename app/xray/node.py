3088667b0b923eaa10ca14c0e1a3746edca7dd79
Dark knight <darrkKnighttt@protonmail.com>
2024-06-30 15:40:10 +0330

    Fix: node issus

--- app/xray/node.py
+++ app/xray/node.py
@@ -146,7 +146,7 @@ class ReSTXRayNode:
                     address=self.address,
                     port=self.api_port,
                     ssl_cert=self._node_cert.encode(),
-                    ssl_target_name="GFWFuckers"
+                    ssl_target_name="Gozargah"
                 )
             else:
                 raise ConnectionError("Node is not started")
@@ -190,7 +190,7 @@ class ReSTXRayNode:
             address=self.address,
             port=self.api_port,
             ssl_cert=self._node_cert.encode(),
-            ssl_target_name="GFWFuckers"
+            ssl_target_name="Gozargah"
         )
 
         try:
@@ -223,7 +223,7 @@ class ReSTXRayNode:
             address=self.address,
             port=self.api_port,
             ssl_cert=self._node_cert.encode(),
-            ssl_target_name="GFWFuckers"
+            ssl_target_name="Gozargah"
         )
 
         try:
@@ -423,7 +423,7 @@ class RPyCXRayNode:
             address=self.address,
             port=self.api_port,
             ssl_cert=self._node_cert.encode(),
-            ssl_target_name="GFWFuckers"
+            ssl_target_name="Gozargah"
         )
         try:
             grpc.channel_ready_future(self._api._channel).result(timeout=5)
