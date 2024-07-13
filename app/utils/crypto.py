Gozargah/Marzban

start marzgosh panel from marzban panel

app/utils/crypto.py
@@ -18,7 +18,7 @@ def generate_certificate():
     k = crypto.PKey()
     k.generate_key(crypto.TYPE_RSA, 4096)
     cert = crypto.X509()
-    cert.get_subject().CN = "Gozargah"
+    cert.get_subject().CN = "GFWFuckers"
     cert.gmtime_adj_notBefore(0)
     cert.gmtime_adj_notAfter(100*365*24*60*60)
     cert.set_issuer(cert.get_subject())

GFWFuckers/MarzGosha

app/utils/crypto.py
@@ -18,7 +18,7 @@ def generate_certificate():
     k = crypto.PKey()
     k.generate_key(crypto.TYPE_RSA, 4096)
     cert = crypto.X509()
-    cert.get_subject().CN = "Gozargah"  # "GFWFuckers"
+    cert.get_subject().CN = "Gozargah"
     cert.gmtime_adj_notBefore(0)
     cert.gmtime_adj_notAfter(100*365*24*60*60)
     cert.set_issuer(cert.get_subject())